d3d613c5e264b976e02ae74100d5e40ce77fca50
atheeq_2003 <atheequrrahaman8@gmail.com>
2023-10-24 04:42:04 +0530

    Complete Form...

--- regform.js
+++ regform.js
@@ -3,8 +3,6 @@ const { Pool } = require('pg');
 const app = express();
 const port = 3000; // Set your desired port number
 const bcrypt = require('bcrypt'); // Import the bcrypt library
-const ejs = require('ejs');
-app.set('view engine', 'ejs');
 
 // Middleware to parse POST data
 app.use(express.urlencoded({ extended: false }));
@@ -40,36 +38,18 @@ app.get('/reset-password', (req, res) => {
 
 app.get('/dashboard', (req, res) => {
     // Render the dashboard page or serve the dashboard HTML here
-    res.send('Welcome to the dashboard!');
+    res.sendFile(__dirname + '/dashboard.html');
 });
 
-app.get('/dashboard', (req, res) => {
-    // Check if the user has a session (i.e., if they are authenticated)
-    if (req.session.username) {
-        // Fetch user-specific data from the database
-        const username = req.session.username;
+app.get('/reset-success', (req, res) => {
+    res.sendFile(__dirname + '/reset-success.html');
+});
 
-        const query = 'SELECT name FROM fam_members WHERE username = $1';
-        const values = [username];
 
-        pool.query(query, values, (err, result) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Error fetching user data.');
-            } else {
-                if (result.rows.length > 0) {
-                    // User-specific data found
-                    const user = result.rows[0];
-                    res.send(`Welcome to the dashboard, ${user.name}!`);
-                } else {
-                    res.status(404).send('User not found.');
-                }
-            }
-        });
-    } else {
-        // User is not authenticated, redirect to the login page
-        res.redirect('/login');
-    }
+
+// Define a route to render the logout success page
+app.get('/logout', (req, res) => {
+    res.sendFile(__dirname + '/logout.html');
 });
 
 
@@ -158,11 +138,20 @@ app.post('/reset-password', async (req, res) => {
 
     try {
         await pool.query(updateQuery, updateValues);
-        res.send('Password reset successful!'); // You can redirect to a confirmation page instead.
+        res.redirect('/reset-success'); // You can redirect to a confirmation page instead.
     } catch (error) {
         console.error('Error:', error);
         res.status(500).send('Error during password reset.');
     }
+});
+
+// Define a route to handle the POST request for logout
+app.post('/logout', (req, res) => {
+    // Perform logout actions here, such as clearing the user's session
+    // Example: req.session.destroy(); // If you are using express-session for session management
+
+    // Redirect to the logout success page
+    res.redirect('/logout');
 });
 
 app.listen(port, () => {
